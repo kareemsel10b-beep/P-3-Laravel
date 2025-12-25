008d5c4dea57218b1ed675a7972106045ec4e394
karim-selam <kareemsel10b@gmail.com>
2025-12-26 01:59:30 +0200

    Fix asset paths and restructure dashboard folders

---
+++ resources/views/admin-3/include-3/js-3.blade.php
@@ -0,0 +1,15 @@
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('dashboard/lib/easing/easing.min.js')}}"></script>
+    <script src="{{ asset('dashboard/lib/waypoints/waypoints.min.js')}}"></script>
+    <script src="{{ asset('dashboard/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+    <script src="{{ asset('dashboard/lib/tempusdominus/js/moment.min.js')}}"></script>
+    <script src="{{ asset('dashboard/lib/tempusdominus/js/moment-timezone.min.js')}}"></script>
+    <script src="{{ asset('dashboard/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="{{ asset('dashboard/mail/jqBootstrapValidation.min.js')}}"></script>
+    <script src="{{ asset('dashboard/mail/contact.js')}}"></script>
+
+    <!-- Template Javascript -->
+    <script src="{{ asset('dashboard/js/main.js')}}"></script>
---
+++ resources/views/layout-3/admin-3/coff.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('admin-3.include-3.head-3')
+</head>
+
+<body>
+    <!-- Navbar Start -->
+    @include('admin-3.include-3.nav-3')
+    <!-- Navbar End -->
+
+    <!-- Carousel Start -->    
+    @include('admin-3.include-3.caro-3')
+    <!-- Carousel End -->
+
+
+    <!-- About Start -->
+    @include('admin-3.include-3.about-3')
+    <!-- About End -->
+
+
+    <!-- Service Start -->
+    @include('admin-3.include-3.serv-3')
+    <!-- Service End -->
+
+
+    <!-- Offer Start -->
+    @include('admin-3.include-3.offer-3')
+    <!-- Offer End -->
+
+
+    <!-- Menu Start -->
+    @include('admin-3.include-3.menu-3')
+    <!-- Menu End -->
+
+
+    <!-- Reservation Start -->
+    @include('admin-3.include-3.reser-3')
+    <!-- Reservation End -->
+
+
+    <!-- Testimonial Start -->
+    @include('admin-3.include-3.test-3')
+    <!-- Testimonial End -->
+
+
+    <!-- Footer Start -->
+    @include('admin-3.include-3.footer-3')
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    @include('admin-3.include-3.js-3')
+
+</body>
+
+</html>
